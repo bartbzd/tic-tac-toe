3e822e1c1a64bbaacb3978314729b0d3b087184f
bartbzd <bbart318@gmail.com>
2021-12-01 22:37:13 -0600

    Organize gameboard and render ttt cells

--- app.js
+++ app.js
@@ -1,55 +1,73 @@
 //PLAYER FACTORY
 const player = (name) => {
-  let player = "Player 1"
-
-  return { player }
+  return { name }
 }
+//DOM SELECTOR MODULE
+const DOM = (() => {
+  const p1 = document.querySelector("#one")
+  const p2 = document.querySelector("#two")
+  const display = document.querySelector(".gameboard")
 
+  return {
+    p1,
+    p2,
+    display,
+  }
+})()
 //GAMEBOARD MODULE
 const gameBoard = (() => {
-  let cell = 0
-  let _xCell = 1
-  let _oCell = 2
+  let boardArr = ["x", "o", "x", "o", "", "", "", "", ""]
 
-  let board = ["x", "o", "", "", "", "", "", "", ""]
-
-  const gameDisplay = document.querySelector(".gameboard")
-
-  for (let i = 0; i < board.length; i++) {
-    let cell = document.createElement("div")
-    cell.classList.add("game-cell")
-    gameDisplay.appendChild(cell)
-    cell.textContent = board[i]
-  }
-
-  const firstPlayerMove = () => {
-    cell = _xCell
-  }
-  const secondPlayerMove = () => {
-    cell = _oCell
-  }
-
-  return { board, firstPlayerMove, secondPlayerMove }
+  return { boardArr }
 })()
 
+//DISPLAY CONTROLLER MODULE
+const displayController = (() => {
+  let player1 = player("Player 3")
+  let player2 = player("Player 2")
+  DOM.p1.textContent = player1.name
+  DOM.p2.textContent = player2.name
+
+  let board = gameBoard.boardArr
+  let i = 0
+
+  const createCell = () => {
+    let cell = document.createElement("div")
+    cell.classList.add("game-cell")
+    DOM.display.appendChild(cell)
+    cell.textContent = board[i]
+  }
+  const render = () => {
+    board.forEach((cell) => {
+      createCell(cell)
+      i++
+    })
+  }
+  const resetBoard = () => {
+    let cell = document.querySelectorAll(".game-cell")
+    gameBoard.boardArr = ["", "", "", "", "", "", "", "", ""]
+    // DOM.display.deleteChild(cell)
+  }
+
+  render()
+  const cells = document.querySelectorAll(".game-cell")
+  cells.forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+      e.target.textContent = "X"
+      resetBoard()
+    })
+  })
+  return {
+    player1,
+    player2,
+    render,
+    board,
+  }
+})()
 //GAME MODULE
-// const gameControl = (() => {
-//   const playerOne = player(Player)
-//   const playerTwo = player(Player)
-
-//   // Loop? board.length
-// })()
-
-// const selection = (board) => {
-//    playerChoice = e.target;
-//    board (Array method /indexOf)
-
-// }
-
-//DOM DISPLAY MODULE
-// const displayGame = (() => {
-// _Query Selectors
-// Render
-// })();
-
-// playerPick.addEventListener("click", selection)
+const gameControl = (() => {
+  //initial state/turn of game
+  //selection clicked, change turns and symbol
+  //first to win or check if board filled(draw)
+  let gameTurn = 0
+})()
