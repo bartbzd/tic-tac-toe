dccbbb375f0d039d4025b3f7fae722338945bd4d
bartbzd <bbart318@gmail.com>
2021-12-06 02:25:29 -0600

    Add marker animations

--- app.js
+++ app.js
@@ -34,44 +34,77 @@ const displayController = (() => {
   const createBoard = () => {
     gameBoard.boardArr.forEach((cell) => {
       createCell(cell)
-
+      document.querySelector(".orange").classList.add("marker-an")
       const cells = document.querySelectorAll(".game-cell")
       cells.forEach((cell, index) => {
         cell.addEventListener("click", (e) => {
-          if (cell.textContent === "") {
-            gameControl.addMark(e)
-            gameControl.changeTurn()
-          }
+          changeTurn(e)
           gameBoard.boardArr.splice(index, 1, e.target.textContent)
         })
       })
     })
   }
 
-  const resetBtn = document.querySelector(".reset")
-  resetBtn.addEventListener("click", () => {
+  const changeTurn = (e) => {
+    if (e.target.textContent === "") {
+      gameControl.addMark(e)
+      gameControl.swapTurn()
+      gameControl.getTurn()
+      console.log(gameControl.getTurn())
+      displayTurn()
+    }
+  }
+
+  const displayTurn = () => {
+    const orange = document.querySelector(".orange")
+    const pink = document.querySelector(".pink")
+    console.log(orange, pink)
+    if (gameControl.getTurn() === 0) {
+      orange.classList.add("marker-an")
+      orange.style.visibility = "visible"
+      pink.style.visibility = "hidden"
+      pink.classList.remove("marker-an")
+    } else if (gameControl.getTurn() === 1) {
+      pink.classList.add("marker-an")
+      pink.style.visibility = "visible"
+      orange.style.visibility = "hidden"
+      orange.classList.remove("marker-an")
+    }
+  }
+  const resetGame = () => {
     gameBoard.resetBoard()
+    gameControl.resetTurn()
+    displayTurn()
     deleteBoard()
     createBoard()
-  })
-  return { createBoard }
+  }
+  const resetBtn = document.querySelector(".reset-btn")
+  resetBtn.addEventListener("click", resetGame)
+
+  return { createBoard, displayTurn }
 })()
 
 //GAME MODULE
 const gameControl = (() => {
   let turn = 0
 
-  const changeTurn = () => {
-    turn === 0 ? (turn = 1) : (turn = 0)
-  }
-
   const addMark = (e) => {
     turn === 0 ? (e.target.textContent = "X") : (e.target.textContent = "O")
   }
+  const swapTurn = () => {
+    turn === 0 ? (turn = 1) : (turn = 0)
+  }
 
+  const getTurn = () => {
+    return turn
+  }
+
+  const resetTurn = () => {
+    return (turn = 0)
+  }
   //first to win or check if board filled(draw)
 
-  return { changeTurn, addMark }
+  return { swapTurn, addMark, getTurn, resetTurn }
 })()
 
 displayController.createBoard()
